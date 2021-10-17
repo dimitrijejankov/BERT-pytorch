096f843a62fde66cec37900f7db32a32442ff350
Dimitrije Jankov <dimitrijejankov@gmail.com>
2021-10-17 17:51:12 -0500

    Generates all the model

--- bert_pytorch/test.py
+++ bert_pytorch/test.py
@@ -10,20 +10,19 @@ bert = BERT()
 
 batch_size = 1
 seq_len = 20
-num_heads = 12
 hidden_layer_size = 768
 
-x = torch.rand(batch_size, seq_len, hidden_layer_size)
-mask = torch.rand(batch_size, seq_len, seq_len)
+for num_heads in range(12, 40):
+    x = torch.rand(batch_size, seq_len, hidden_layer_size)
+    mask = torch.rand(batch_size, seq_len, seq_len)
 
+    for param_tensor in bert.state_dict():
+        print(param_tensor, "\t", bert.state_dict()[param_tensor].size())
 
-for param_tensor in bert.state_dict():
-    print(param_tensor, "\t", bert.state_dict()[param_tensor].size())
+    torch.save(bert.state_dict(), "./bert.pth")
+    bert.load_state_dict(torch.load("./bert.pth"))
+    bert.eval()
 
-torch.save(bert.state_dict(), "./bert.pth")
-bert.load_state_dict(torch.load("./bert.pth"))
-bert.eval()
-
-
-module = torch.jit.trace(bert, (x, mask)).cpu()
-torch.jit.save(module, 'scriptmodule.pt')
+    module = torch.jit.trace(bert, (x, mask)).cpu()
+    torch.jit.save(module, 'bert-%s-heads-%s-hidden.pt' %
+                   (num_heads, hidden_layer_size))
